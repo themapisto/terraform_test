97549d93d3fee8a33ae98ec6b7f4788917926cf6
themapisto <themapisto@naver.com>
2024-04-04 16:03:32 +0900

    vmc terraform

--- main.tf
+++ main.tf
@@ -1,42 +1,41 @@
-provider "aws" {
-    version = "2.69.0"
-    region="us-west-1"
-}
-
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-variable "myTag" {
-  description = "My Input Tag"
-  default = "terraform-test"
-}
-
-resource "aws_instance" "machine1" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = "t2.micro"
-    availability_zone = "us-west-1b"
-    tags = {
-      "type" = var.myTag
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.4.1"
     }
+  }
 }
 
-resource "aws_instance" "machine2" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = "t2.micro"
-    availability_zone = "us-west-1b"
-    tags = {
-      "type" = var.myTag
-    }
+provider "vsphere" {
+  user                 = "cloudadmin@vmc.local"
+  password             = "g+DOXyp08z!IZIn"
+  vsphere_server       = "10.5.1.196"
+  allow_unverified_ssl = true
 }
 
-#resource "aws_network_interface_sg_attachment" "sg_attachment1" {
-#  security_group_id    = "sg-8e01a2fb"
-#  network_interface_id = "${aws_instance.machine1.primary_network_interface_id}"
-#}
+data "vsphere_datacenter" "datacenter" {
+  name = "SDDC-Datacenter"
+}
 
-#resource "aws_network_interface_sg_attachment" "sg_attachment2" {
-#  security_group_id    = "sg-aa750ddc"
-#  network_interface_id = "${aws_instance.machine2.primary_network_interface_id}"
-#}
+data "vsphere_compute_cluster" "cluster" {
+  name          = "Cluster-1"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+
+data "vsphere_datastore" "datastore" {
+  name          = "WorkloadDatastore"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+
+data "vsphere_network" "network" {
+  name          = "SEG-30-20-10"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "VMe"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
---
+++ vm.tf
@@ -0,0 +1,30 @@
+resource "vsphere_virtual_machine" "vm" {
+  name             = "koo-terraform-vsphere"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 2
+  guest_id         = "centos7_64Guest"
+  memory           = 2048
+  firmware = "efi"
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk"
+    size  = 50
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = "hello-world"
+        domain    = "vmwarevmc.com"
+      }
+      network_interface {
+        ipv4_address = "30.20.10.165"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "30.20.10.1"
+    }
+  }
+}
