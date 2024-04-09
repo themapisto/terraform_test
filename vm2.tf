136d58be38201c8b7a728409050459bd2cef4c5f
themapisto <themapisto@naver.com>
2024-04-09 18:13:49 +0900

    re

---
+++ vm2.tf
@@ -0,0 +1,15 @@
+resource "vsphere_virtual_machine" "vm" {
+  name             = "foo"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "other3xLinux64Guest"
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
